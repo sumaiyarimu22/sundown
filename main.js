76cdbcfae2eec37ad331ff0d0db5f388d15e178b
sumaiyarimu22 <sumaiyarimu22@gmail.com>
2024-05-24 20:48:11 +0600

    done

--- main.js
+++ main.js
@@ -25,3 +25,11 @@ function featuredAnimation() {
 }
 
 featuredAnimation();
+
+function loaderAnimation() {
+  var loader = document.querySelector("#loader");
+  setTimeout(function () {
+    loader.style.top = "-100%";
+  }, 4200);
+}
+loaderAnimation();
